be75a783f0fa608cb9810813374326a1fb008ae2
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-12-17 23:03:03 +0700

    remove duplicates from test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,23 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  base_title = '| Railstutor App'
+
   test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', 'About | Railstutor App'
+    assert_select 'title', "About #{base_title}"
   end
 
   test 'should get help' do
     get static_pages_help_url
     assert_response :success
-    assert_select 'title', 'Help | Railstutor App'
+    assert_select 'title', "Help #{base_title}"
   end
 
   test 'should get home' do
     get static_pages_home_url
     assert_response :success
-    assert_select 'title', 'Home | Railstutor App'
+    assert_select 'title', "Home #{base_title}"
   end
 end
