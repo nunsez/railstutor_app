a0ef80e2d9ee924e9513ddce9ecd729f2fe9b1bc
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-12-18 23:22:44 +0700

    add nav classes helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
     return base_title if page_title.empty?
     return sanitize "#{page_title} | #{base_title}"
   end
+
+  def nav_classes(nav = nil)
+    base_classes = 'nav-link'
+
+    nav == params[:action] ? "#{base_classes} active" : base_classes
+  end
 end
