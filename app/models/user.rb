9ea54fb29d1c73f37d7c09e86cbb2c0065d3f12b
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-12-19 18:19:07 +0700

    add downcase email before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: URI::MailTo::EMAIL_REGEXP },
                     uniqueness: { case_sensitive: false }
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
