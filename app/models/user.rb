231b8833027cb327b769a1f7716f0829331943bc
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-12-19 18:04:03 +0700

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP },
+                    uniqueness: { case_sensitive: false }
 end
