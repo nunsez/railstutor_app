34ba171eaa84081e61e68617229e4fd2244e5c78
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-12-19 18:07:59 +0700

    switch back to default minitest reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'rails/test_help'
 require 'minitest/reporters'
 
 Minitest::Reporters.use!(
-  Minitest::Reporters::SpecReporter.new,
+  Minitest::Reporters::DefaultReporter.new,
   ENV,
   Minitest.backtrace_filter
 )
