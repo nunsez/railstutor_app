bffc4e3f37b60cb1804084d85a27c29c457a3c90
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-12-22 22:52:37 +0700

    update signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,13 +4,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'invalid signup information' do
     get signup_path
 
-    assert_no_difference 'User.count' do
+    assert_no_difference -> { User.count } do
       post users_path, params: { user: { name: '',
                                          email: 'user@invalid',
                                          password: 'foo',
                                          password_confirmation: 'bar' }}
     end
 
+    assert_equal 200, status
+    assert_equal users_path, path 
     assert_select 'h1', text: 'Sign up'
   end
 end
