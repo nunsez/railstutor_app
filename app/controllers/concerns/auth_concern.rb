a521df231686fd2b282f78583681618895524122
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-12-25 18:38:52 +0700

    add auth concern

---
+++ app/controllers/concerns/auth_concern.rb
@@ -0,0 +1,22 @@
+module AuthConcern
+  def id = :user_id
+
+  def log_in(user)
+    session[id] = user.id
+  end
+
+  def log_out
+    session.delete id
+    session.clear
+  end
+
+  def logged_in? = !current_user.guest?
+  
+  def current_user
+    @current_user ||= User.find_by_id(session[id]) || Guest.new
+  end
+
+  def authenticate_user!
+    redirect_to login_path unless logged_in?
+  end
+end
