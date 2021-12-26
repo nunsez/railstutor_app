026d2a63fbf30ad3f8584053c7138e70eb8d4e76
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-12-26 11:55:32 +0700

    fix comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
-                    # format: { with: URI::MailTo::EMAIL_REGEXP }, # bad validation: user@invalid marks as valid
+                    # format: { with: URI::MailTo::EMAIL_REGEXP }, # bad validation: 'user@invalid' marks as valid
                     format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
