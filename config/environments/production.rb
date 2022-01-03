0f7382568b61af0c3f216f3a196a4062c28e2f10
Alexander Mandrikov <mad.nunsez@gmail.com>
2022-01-03 22:49:58 +0700

    fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,12 +74,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'nunsez-railstutorial.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {
-    host: 'localhost:3000'
-  }
-
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
