32bcc035933bc08bcc7f2cf2c6598a51e04e7da9
Alexander Mandrikov <mad.nunsez@gmail.com>
2022-01-06 13:03:46 +0700

    improve routes paging

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     get '(page/:page)', action: :index, on: :collection, as: ''
   end
 
-  root 'static_pages#home'
+  # root 'static_pages#home'
+  get '/(posts/:page)', to: 'static_pages#home', as: :root
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
@@ -14,7 +15,13 @@ Rails.application.routes.draw do
   get 'help', to: 'static_pages#help'
   get 'signup', to: 'users#new'
 
-  resources :users, except: %i[new], concerns: :paginatable
+  resources :users, except: %i[new], concerns: :paginatable do
+    member do
+      get :following, :followers
+      get '(posts/:page)', action: :show
+    end
+  end
+
   resources :account_activations, only: :edit
   resources :password_resets, only: %i[new create edit update], param: :token
   resources :microposts, only: %i[create destroy]
